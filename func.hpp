b16b7c17c2e0d80ec37feda42623672fedde4ca7
pswoonie <swp8111@gmail.com>
2024-03-28 17:19:01 +0900

    [UPDATE] add ifndef in header file

---
+++ func.hpp
@@ -0,0 +1,13 @@
+// This is header file
+
+#ifndef FUNC_HPP
+#define FUNC_HPP
+
+#include <iostream>
+
+using namespace std;
+// using std::cin;
+// using std::cout;
+// using std::endl;
+
+#endif /* FUNC_HPP */
